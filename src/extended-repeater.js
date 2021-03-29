acec89cf9b8b46e94a8cf9052a18654b128d2d17
Katya Litvinchuk <katyalitvinchuk.1@gmail.com>
2021-03-29 22:52:20 +0300

    fix: update the implementation

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+  if (!options.separator) {
+    options.separator = "+";
+  }
+  if (typeof options.addition === "undefined") {
+    options.addition = "";
+  }
+  if (!options.additionRepeatTimes) {
+    options.additionRepeatTimes = 1;
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = "|";
+  }
+
+  if (typeof str !== "string") {
+    if (str === null) {
+      defaultStr = "null"
+    } else {
+      defaultStr = str.toString()
+    }
+
+  } else {
+    defaultStr = str;
+  }
+  if (typeof options.addition !== "string") {
+
+    if (options.addition === null) {
+      options.addition = "null";
+    } else {
+      options.addition = options.addition.toString();
+    }
+  }
+
+  let resultString = "";
+  for (let i = 0; i < options.repeatTimes; i++) {
+    if (i !== 0) {
+      resultString += options.separator;
+    }
+    resultString += defaultStr;
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      if (j !== 0) {
+        resultString += options.additionSeparator;
+      }
+      resultString += options.addition;
+    }
+  }
+  return resultString;
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,75 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(machineType) {
+    if (machineType !== false) {
+      this.machineType = "direct";
+    } else {
+      this.machineType = "reverse";
+    }
+    this.table = new Map();
+    for (let codeRow = 65; codeRow < 91; codeRow++) {
+      let row = new Map();
+      for (let codeCol = codeRow; codeCol < codeRow + 26; codeCol++) {
+        const newCode = codeCol % 91 < 65 ? (codeCol % 91) + 65 : codeCol;
+        row.set(
+          String.fromCharCode(codeCol - codeRow + 65),
+          String.fromCharCode(newCode)
+        );
+      }
+      this.table.set(String.fromCharCode(codeRow), row);
+    }
   }
-}
+  encrypt(message, key) {
+    let result = [];
+    if (!(message && key)) {
+      throw "Error";
+    }
+    let keyIndex = 0;
+    for (let v of message) {
+      if (
+        !(v.charCodeAt(0) < 97 && v.charCodeAt(0) > 90) &&
+        v.charCodeAt(0) >= 65 &&
+        v.charCodeAt(0) <= 122
+      ) {
+        result.push(
+          this.table
+            .get(v.toUpperCase())
+            .get(key[keyIndex % key.length].toUpperCase())
+        );
+        keyIndex++;
+      } else {
+        result.push(v);
+      }
+    }
+    if (this.machineType === "reverse") result.reverse();
+    return result.join("");
+  }
+  decrypt(encryptedMessage, key) {
+    let result = [];
+    if (!(encryptedMessage && key)) {
+      throw "Error";
+    }
+    let keyIndex = 0;
+    for (let v of encryptedMessage) {
+      if (
+        !(v.charCodeAt(0) < 97 && v.charCodeAt(0) > 90) &&
+        v.charCodeAt(0) >= 65 &&
+        v.charCodeAt(0) <= 122
+      ) {
+        const row = this.table.get(key[keyIndex % key.length].toUpperCase());
+        for (let k of row.keys()) {
+          if (row.get(k) === v.toUpperCase()) {
+            result.push(k);
+            break;
+          }
+        }
+        keyIndex++;
+      } else result.push(v.toUpperCase());
+    }
+    if (this.machineType === "reverse") result.reverse();
+    return result.join("");
+  }
+};
 
 module.exports = VigenereCipheringMachine;
