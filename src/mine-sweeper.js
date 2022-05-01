742c45e22c0d1516fa9d46b10516d1bee6219dba
Katya Litvinchuk <katyalitvinchuk.1@gmail.com>
2022-05-01 18:31:43 +0300

    feat: add solution

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,9 +1,55 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 
-function minesweeper(matrix) {
-  
-}
+const minesweeper = matrix => {
+  let result = [];
+
+  matrix.forEach((row, i) => {
+    result.push([]);
+
+    row.forEach((cell, j) => {
+      result[i].push(0);
+
+      if (i > 0) {
+        if (j > 0 && matrix[i - 1][j - 1]) {
+          result[i][j] += 1;
+        }
+        
+        if (matrix[i - 1][j]) {
+          result[i][j] += 1;
+        }
+
+        if (j < row.length - 1 && matrix[i - 1][j + 1]) {
+          result[i][j] += 1;
+        }
+      }
+
+      if (i < matrix.length - 1) {
+        if (j > 0 && matrix[i + 1][j - 1]) {
+          result[i][j] += 1;
+        }
+
+        if (matrix[i + 1][j]) {
+          result[i][j] += 1;
+        }
+
+        if (j < row.length - 1 && matrix[i + 1][j + 1]) {
+          result[i][j] += 1;
+        }
+      }
+
+      if (j > 0 && matrix[i][j - 1]) {
+        result[i][j] += 1;
+      }
+
+      if (j < row.length - 1 && matrix[i][j + 1]) {
+        result[i][j] += 1;
+      }
+    });
+  });
+
+  return result;
+};
 
 module.exports = {
   minesweeper
