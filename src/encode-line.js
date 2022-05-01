f3f8df9732dc005685a524e6cf92b5edb8bc89a6
Katya Litvinchuk <katyalitvinchuk.1@gmail.com>
2022-05-01 15:45:34 +0300

    feat: add solution

--- src/encode-line.js
+++ src/encode-line.js
@@ -3,14 +3,15 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 function encodeLine(str) {
   let result = "";
-  let count = 1;
-  for (let i = 0; i < str.split("").length; i++) {
+    for (let i = 0; i < str.split("").length; i++) {
+      let count = 1;
     while (str[i] === str[i + 1]) {
       count++;
+      i++;
     }
     count > 1 ? (result += count + str[i]) : (result += str[i]);
   }
-  return result.join("");
+  return result;
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,19 +1,15 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Given an array with heights, sort them except if the value is -1.
- *
- * @param {Array} arr
- * @return {Array}
- *
- * @example
- * arr = [-1, 150, 190, 170, -1, -1, 160, 180]
- *
- * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
- */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+function sortByHeight(arr) {
+  let sort=arr.filter(e=> e!==-1).sort((a,b)=>a-b);
+   for (let i=0; i<arr.length; i++){
+     if (arr[i]==-1){
+        sort.splice(i, 0, arr[i]);
+   }
+ }
+  return sort;
 }
 
 module.exports = {
